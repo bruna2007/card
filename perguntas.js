3413fd140f9516e92edabec2fe2148ed5d3ec2a8
bruna2007 <151949533+bruna2007@users.noreply.github.com>
2024-10-17 12:15:22 -0300

    17/10

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'programaçaão',
+    'o que é python?',
+    'o python é uma linguagem de progração'
+)
+
+criacartao(
+    'geografia',
+    'qual a capital da frança é paris'
+
+)
+
+criacartao(
+    "programação",
+    'oq é uma função?',
+    'uma função é um bloco de código que executa algum tarefa'
+)
+
+criacartao(
+    'lingua inglesa',
+    ' como se diz oi em inglẽs?',
+    'oi em inglẽs é HI (RAI)'
+)
